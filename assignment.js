969256879aeea0df553c5538b0e99e468f5d8055
AsifRKabir <asifrkabir20@gmail.com>
2020-07-17 00:23:13 +0600

    additions for negative cases

--- assignment.js
+++ assignment.js
@@ -1,39 +1,55 @@
 //feetToMile
 
-function feetToMile(feet){
-    var mile = feet * 0.000189394;
-    var output = feet + " feet is equal to " + mile + " mile.";
+function feetToMile(feet) {
+    var output = "";
+    if (feet < 0) {
+        output = "Distance cannot be negative.";
+    }
+    else {
+        var mile = feet * 0.000189394;
+        output = feet + " feet is equal to " + mile + " mile.";
+    }
     return output;
 }
+
+
 
 //woodCalculator
 
-function woodCalculator(chair, table, bed){
-    var totalWood = chair + table * 3 + bed * 5;
-    var output = "You need " + totalWood + " cubic feet of wood.";
+function woodCalculator(chair, table, bed) {
+    var output = "";
+    if (chair < 0 || table < 0 || bed < 0) {
+        output = "Invalid input. Please enter another number.";
+    }
+    else {
+        var totalWood = chair + table * 3 + bed * 5;
+        output = "You need " + totalWood + " cubic feet of wood.";
+    }
     return output;
 }
 
+
+
 //brickCalculator
 
-function brickCalculator(floors){
+function brickCalculator(floors) {
     var totalBrick = 0;
     var brickArr = [];
     var output = "";
-    if(floors <= 0){
-        output = "Please enter another number.";
+    if (floors <= 0) {
+        output = "Floors cannot be zero or negative.";
     }
-    else{
-        if(floors <= 10){
+    else {
+        if (floors <= 10) {
             brickArr.push(floors);
             totalBrick = brickArr[0] * 15 * 1000;
         }
-        else if(floors <= 20){
+        else if (floors <= 20) {
             brickArr[0] = 10;
             brickArr[1] = floors - 10;
             totalBrick = brickArr[0] * 15 * 1000 + brickArr[1] * 12 * 1000;
         }
-        else if(floors > 20){
+        else if (floors > 20) {
             brickArr[0] = 10;
             brickArr[1] = 10;
             brickArr[2] = floors - 20;
@@ -44,17 +60,27 @@ function brickCalculator(floors){
     return output;
 }
 
+
+
 //tinyFriend
 
-function tinyFriend(friendArr){
-    var tinyFriend = friendArr[0];
-    var min = friendArr[0].length;
-    for(var i = 1; i < friendArr.length; i++){
-        if(friendArr[i].length < min){
-            min = friendArr[i].length;
-            tinyFriend = friendArr[i];
-        }
+function tinyFriend(friendArr) {
+    var output = "";
+    if (friendArr.length == 0) {
+        output = "The array is empty. Please enter another array.";
     }
-    var output = tinyFriend + " has the smallest name of length " + min;
+    else {
+        var tinyFriend = friendArr[0];
+        var min = friendArr[0].length;
+        for (var i = 1; i < friendArr.length; i++) {
+            if (friendArr[i].length < min) {
+                min = friendArr[i].length;
+                tinyFriend = friendArr[i];
+            }
+        }
+        output = tinyFriend + " has the smallest name of length " + min;
+    }
     return output;
 }
+
+console.log(tinyFriend(["sam", "Mo", "Richard"]));
